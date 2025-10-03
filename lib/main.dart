11bbc36b2d8ad4a2d164a50ff07da222ff8bc625
twelvekun <sadadarguas@gmail.comtwelvekunssh-keygen -t ed25519 -C sadadarguas@gamil.com>
2025-10-03 13:55:45 +0700

    chore: initial commit flutter app

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const Center(
+      child: Text(
+        'Hello, world!',
+        textDirection: TextDirection.ltr,
+        style: TextStyle(color: Colors.blue),
+      ),
+    ),
+  );
+}
